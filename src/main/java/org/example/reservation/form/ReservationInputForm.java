b20209779cbd31638f51f77635ed9d0765f75bca
3030745 <3030745@LAPTOP-QT1OQOIG>
2024-03-21 14:48:20 +0900

    aaa

--- src/main/java/org/example/reservation/form/ReservationInputForm.java
+++ src/main/java/org/example/reservation/form/ReservationInputForm.java
@@ -1,8 +1,14 @@
 package org.example.reservation.form;
 
 import java.time.LocalDateTime;
-import jakarta.validation.constraints.*;
+
 import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,7 +22,7 @@ public class ReservationInputForm {
 	//商品ID
 private Integer id;
 
-//↓の各変数(name,phone,email,date)にはreservation.htmlから貰ってきた値を格納される
+//↓の各変数(name,phone,email,date)にはreservation.htmlから貰ってきた値が格納される
 @NotBlank//文字列が空かどうか確認する
 @NotNull
 private String name;
